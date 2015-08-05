a393afb952c443c192fc0d2993071e7e8ac4e858
Jérôme Nadaud <jerome.nadaud@prestashop.com>
2015-08-05 09:30:08 +0200

    // Version updated to v1.4.0

--- statssearch.php
+++ statssearch.php
@@ -37,7 +37,7 @@ class StatsSearch extends ModuleGraph
 	{
 		$this->name = 'statssearch';
 		$this->tab = 'analytics_stats';
-		$this->version = '1.3.2';
+		$this->version = '1.4.0';
 		$this->author = 'PrestaShop';
 		$this->need_instance = 0;
 
